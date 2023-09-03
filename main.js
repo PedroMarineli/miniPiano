a633682b1a06f5581ed4156df352c64d3420d3a2
PedroMarineli <pedrohenrique.marineli@gmail.com>
2023-09-03 19:14:06 +0000

    aplicação dos sons

---
+++ main.js
@@ -0,0 +1,43 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+    console.log(elemento);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.pianoTecla');
+console.log(listaDeTeclas);
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#pianoTecla-${instrumento}`; //template string
+    console.log(tecla);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+        console.log(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
