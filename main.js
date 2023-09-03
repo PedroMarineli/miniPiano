0d923a0e80064530a5036bdcd01933a5c0f5b63e
PedroMarineli <pedrohenrique.marineli@gmail.com>
2023-09-03 19:52:08 +0000

    aplicando as teclas

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function tocaSom (seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
-    console.log(elemento);
+    //console.log(elemento);
 
     if (elemento && elemento.localName === 'audio') {
         elemento.play();
@@ -13,7 +13,7 @@ function tocaSom (seletorAudio) {
 }
 
 const listaDeTeclas = document.querySelectorAll('.pianoTecla');
-console.log(listaDeTeclas);
+//console.log(listaDeTeclas);
 
 //para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
@@ -21,23 +21,60 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#pianoTecla-${instrumento}`; //template string
-    console.log(tecla);
 
     tecla.onclick = function () {
         tocaSom(idAudio);
-        console.log(idAudio);
+        //console.log(idAudio);
     }
-
-    tecla.onkeydown = function (evento) {
-
-        if (evento.code === 'Space' || evento.code === 'Enter') {
+  
+    onkeydown = function (evento) {
+        
+        if (evento.code === 'KeyA') {
+            const tecla = listaDeTeclas[0];
             tecla.classList.add('ativa');
+            tocaSom('#pianoTecla-do');
+        }
+        if (evento.code === 'KeyS') {
+            const tecla = listaDeTeclas[1];
+            tecla.classList.add('ativa');
+            tocaSom('#pianoTecla-re');
+        }
+        if (evento.code === 'KeyD') {
+            const tecla = listaDeTeclas[2];
+            tecla.classList.add('ativa');
+            tocaSom('#pianoTecla-mi');
+        }
+        if (evento.code === 'KeyF') {
+            const tecla = listaDeTeclas[3];
+            tecla.classList.add('ativa');
+            tocaSom('#pianoTecla-fa');
+        }
+        if (evento.code === 'KeyG') {
+            const tecla = listaDeTeclas[4];
+            tecla.classList.add('ativa');
+            tocaSom('#pianoTecla-sol');
+        }
+        if (evento.code === 'KeyH') {
+            const tecla = listaDeTeclas[5];
+            tecla.classList.add('ativa');
+            tocaSom('#pianoTecla-la');
+        }
+        if (evento.code === 'KeyJ') {
+            const tecla = listaDeTeclas[6];
+            tecla.classList.add('ativa');
+            tocaSom('#pianoTecla-si');
         }
 
     }
 
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
+   onkeyup = function () {
+        document.querySelector('.do').classList.remove('ativa');
+        document.querySelector('.re').classList.remove('ativa');
+        document.querySelector('.mi').classList.remove('ativa');
+        document.querySelector('.fa').classList.remove('ativa');
+        document.querySelector('.sol').classList.remove('ativa');
+        document.querySelector('.la').classList.remove('ativa');
+        document.querySelector('.si').classList.remove('ativa');
     }
 
 }
